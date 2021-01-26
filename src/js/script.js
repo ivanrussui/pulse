09c8e50d70555b4044d7c2ec17fdda13b6989187
Ivan <fenomen_ny@mail.ru>
2021-01-27 02:38:29 +0300

    js intro

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,72 @@
-// var name = "Ivan";
-// let number = 7;
-// const pi = 3.14;
+var name = "Ivan";
+let number = 7;
+const pi = 3.14;
 
-// number = 4;
-// pi = 5;
+number = 4;
+
+let leftBorderWidth;
+
+
+// number
+// string - "", '', ``
+// true/false
+// null
+// undefined
+// let obj = {
+// 	name: 'apple',
+// 	color: 'green',
+// 	weiht: 200
+// }
+// Symbol
+
+// alert(1234);
+// console.log(number);
+// let answer = confirm('Вам есть 18?');
+// console.log(answer);
+
+// let answer = prompt('Вам есть 18?', '');
+// console.log(answer);
+
+// console.log(4 + 'fgf');
+
+// let isChecked = false,
+// 		islouse = false;
+
+// console.log(isChecked && islouse);
+
+// console.log(isChecked || islouse);
+
+// if (2 * 6 == 8 * 1) {
+// 	console.log('Верно');
+// }	else {
+// 	console.log('Ошибка');
+// }
+
+// let answer = confirm('Вам есть 18?');
+// if (answer) {
+// 	console.log('Проходите');
+// } else {
+// 	console.log('Уходите');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+// 	console.log('Неправильно');
+// }	else if (num > 100) {
+// 	console.log('Много');
+// } else {
+// 	console.log('Верно');
+// }
+
+// for (let i = 1; i < 8; i++) {
+// 	console.log(i);
+// }
+
+function	logging(a, b) {
+	console.log(a + b);
+}
+
+logging(3, 5);
+
+logging(5, 7);
