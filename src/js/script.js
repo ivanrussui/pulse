782d912c8a9fde91b38dbf84914948880c6f548e
Ivan <fenomen_ny@mail.ru>
2021-02-09 13:56:08 +0300

    validate step 2 + responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Modal
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut(450);
+  });
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
 
-	// Modal
-	$('[data-modal=consultation]').on('click', function() {
-		$('.overlay, #consultation').fadeIn('slow');
-	});
-	$('.modal__close').on('click', function() {
-		$('.overlay, #consultation, #order, #thanks').fadeOut(450);
-	});
-	$('.button_mini').each(function(i) {
-		$(this).on('click', function() {
-			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-			$('.overlay, #order').fadeIn('slow');
-		});
-	});
+	// Validate
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: 'required',
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: 'Пожалуйста, введите своё имя',
+          minlength: jQuery.validator.format('Введите более {0} букв!'),
+        },
+        phone: 'Пожалуйста, введите свой номер телефона',
+        email: {
+          required: 'Пожалуйста, введите свою почту',
+          email: 'Неправильно введен адрес почты',
+        },
+      },
+    });
+  }
 
-	$('#consultation-form').validate();
-	$('#consultation form').validate({
-		
-	});
-	$('#order form').validate();
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
 });
