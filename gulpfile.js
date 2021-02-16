514c3c8430e53f13756b7af88e198e2364431c3a
Ivan <fenomen_ny@mail.ru>
2021-02-16 17:56:28 +0300

    pagespeed

--- gulpfile.js
+++ gulpfile.js
@@ -34,42 +34,48 @@ gulp.task('styles', function () {
 
 gulp.task('watch', function () {
   gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
-	gulp.watch('src/*.html').on('change', gulp.parallel('html'));
+  gulp.watch('src/*.html').on('change', gulp.parallel('html'));
   gulp.watch('src/*.html').on('change', browserSync.reload);
-  
 });
 
 gulp.task('html', function () {
-	return gulp.src('src/*.html')
-		.pipe(htmlmin({ collapseWhitespace: true }))
-		.pipe(gulp.dest('dist/'));
+  return gulp
+    .src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('scripts', function () {
-	return gulp.src('src/js/**/*.js')
-		.pipe(gulp.dest('dist/js'));
+  return gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('fonts', function () {
-	return gulp.src('src/fonts/**/*')
-		.pipe(gulp.dest('dist/fonts'));
+  return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('icons', function () {
-	return gulp.src('src/icons/**/*')
-		.pipe(gulp.dest('dist/icons'));
+  return gulp.src('src/icons/**/*').pipe(gulp.dest('dist/icons'));
 });
 
 gulp.task('mailer', function () {
-	return gulp.src('src/mailer/**/*')
-		.pipe(gulp.dest('dist/mailer'));
+  return gulp.src('src/mailer/**/*').pipe(gulp.dest('dist/mailer'));
 });
 
 gulp.task('images', function () {
-	return gulp.src('src/img/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/img'));
+  return gulp.src('src/img/**/*').pipe(imagemin()).pipe(gulp.dest('dist/img'));
 });
 
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'));
+gulp.task(
+  'default',
+  gulp.parallel(
+    'watch',
+    'server',
+    'styles',
+    'scripts',
+    'fonts',
+    'icons',
+    'mailer',
+    'html',
+    'images'
+  )
+);
