1b375461078d5fce77d5a0fe19c5f21eca3c2c33
Ivan <fenomen_ny@mail.ru>
2021-04-06 13:32:11 +0300

    плавность для всех ссылок

--- src/js/script.js
+++ src/js/script.js
@@ -146,7 +146,7 @@ $(document).ready(function () {
   });
 
   // плавность перехода по всем ссылкам на сайте
-  $("a[href='#up']").click(function () {
+  $("a[href^='#']").click(function () {
     const _href = $(this).attr('href');
     $('html, body').animate({ scrollTop: $(_href).offset().top + 'px' });
     return false;
